7b195c5d6f424c1fe57d85d9af0da7510e7b00d5
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-05-12 18:51:54 -0500

    pushing webApiApp edits

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log ("hello!");
+var timeEl = document.querySelector(".time");
+var startBtn = document.getElementById("startBtn");
+var submit = document.getElementById("submit");
+var quiz = document.getElementById("quiz");
+var score = document.getElementById("score");
+var username= document.getElementById("name");
+var usernamein = document.getElementById("usernamein");
+var question = document.getElementById("question");
+var answerA = document.getElementById("A");
+var answerB = document.getElementById("B");
+var answerC = document.getElementById("C");
+var answerD = document.getElementById("D");
+var usernameform =document.getElementById("usernameform")
+var correct = ["red","blue","square","circle"];
+var useranswers = [] ;
+
+var exam = [
+  { q: "What color has the lowest wavelength frequency?",
+      a: 
+          {a:"blue",
+          b:"yellow",
+          c:"red", 
+          d:"orange"} }];
+
+
+
+
+
+
+
+
+
+/*var startBtn = document.getElementById ("startbtn");
+var timeEl = document.querySelector(".time");
+
+var listOfQuestions= 
+startBtn.addEventListener("click", newQuestion)
+
+function newQuestion(){
+  return 3;
+
+  
+}*/
